9268eed98fbc5424463f06047672ebce5b00d2b7
thexeromin <thexeromin@gmail.com>
2024-06-07 09:45:33 +0530

    add: refresh token rotation

--- auth.ts
+++ auth.ts
@@ -1,6 +1,7 @@
-import NextAuth from 'next-auth'
+import NextAuth, { type User } from 'next-auth'
 import Credentials from 'next-auth/providers/credentials'
 import { signinAPI } from './api'
+import { AdapterUser } from 'next-auth/adapters'
 
 export const { handlers, signIn, signOut, auth } = NextAuth({
     providers: [
@@ -23,4 +24,102 @@ export const { handlers, signIn, signOut, auth } = NextAuth({
             },
         }),
     ],
+    callbacks: {
+        async jwt({ token, user }) {
+            if (user && user.profile) {
+                // First login, save the `access_token`, `refresh_token`, and other
+                // details into the JWT
+                const userProfile: User = {
+                    id: token.sub,
+                    name: `${user.profile.firstName} ${user.profile.lastName}`,
+                    email: user.email,
+                    image: user.profile.avatarUrl,
+                }
+
+                return {
+                    accessToken: user.token?.accessToken,
+                    expires_at: token.exp || Math.floor(Date.now() / 1000) + 30,
+                    refreshToken: user.token?.refreshToken,
+                    user: userProfile,
+                }
+            } else if (Date.now() < token.expires_at * 1000) {
+                // Subsequent logins, if the `access_token` is still valid, return the JWT
+                return token
+            } else {
+                // Subsequent logins, if the `access_token` has expired, try to refresh it
+                console.log('refresing token')
+                if (!token.refreshToken)
+                    throw new Error('Missing refresh token')
+
+                try {
+                    // The `token_endpoint` can be found in the provider's documentation. Or if they support OIDC,
+                    // at their `/.well-known/openid-configuration` endpoint.
+                    // i.e. https://accounts.google.com/.well-known/openid-configuration
+                    let headersList = {
+                        Accept: '*/*',
+                        Authorization: `Bearer ${token.refresh_token}`,
+                    }
+                    let response = await fetch(
+                        `${process.env.API}/auth/refresh`,
+                        {
+                            method: 'POST',
+                            headers: headersList,
+                        },
+                    )
+
+                    let data = await response.json()
+                    console.log(data)
+                    return {
+                        // Keep the previous token properties
+                        ...token,
+                        access_token: data.accessToken,
+                        expires_at: Math.floor(Date.now() / 1000) + 30,
+                        // Fall back to old refresh token, but note that
+                        // many providers may only allow using a refresh token once.
+                        refresh_token: data.refreshToken ?? token.refreshToken,
+                    }
+                } catch (error) {
+                    console.error('Error refreshing access token', error)
+                    // The error property can be used client-side to handle the refresh token error
+                    return {
+                        ...token,
+                        error: 'RefreshAccessTokenError' as const,
+                    }
+                }
+            }
+        },
+        session({ session, token }) {
+            if (token.user) {
+                session.user = token.user as User & AdapterUser
+            }
+            return session
+        },
+    },
 })
+
+declare module 'next-auth' {
+    interface User {
+        profile?: {
+            firstName: string
+            lastName: string
+            avatarUrl: string
+        }
+        token?: {
+            accessToken: string
+            refreshToken: string
+        }
+    }
+
+    interface Session {
+        error?: 'RefreshAccessTokenError'
+    }
+}
+
+declare module '@auth/core/jwt' {
+    interface JWT {
+        accessToken?: string
+        expires_at: number
+        refreshToken?: string
+        error?: 'RefreshAccessTokenError'
+    }
+}
